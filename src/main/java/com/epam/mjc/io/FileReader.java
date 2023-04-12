b860c576d329ac472c59c0580dccf8bc7d57cab8
yorqinjon <yorqinjonmahmudov93@gmail.com>
2023-04-12 20:08:36 +0500

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file));) {
+            var profile = new Profile();
+            String s = null;
+            s= reader.readLine();
+            while (s!=null){
+                if (s.contains("Name"))
+                    profile.setName(s.substring(6));
+                else if (s.contains("Age"))
+                    profile.setAge(Integer.valueOf(s.substring(5)));
+                else if (s.contains("Email"))
+                    profile.setEmail(s.substring(7));
+                else if (s.contains("Phone"))
+                    profile.setPhone(Long.valueOf(s.substring(7)));
+                s=reader.readLine();
+            }
+            return profile;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
